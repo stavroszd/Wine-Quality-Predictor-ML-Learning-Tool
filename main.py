0fe91f5fd51a684f1b33a484ab5b8a9651b8b516
Stavros Dimou <stavroszdimou@gmail.com>
2025-02-12 17:09:09 +0200

    Modified for further modularity - expirmemented with Optuna

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ Created on Thu Jan  2 15:24:52 2025
 
 #%%Importing the necessary packages 
 
+#Basic Python Packages
+import os 
+import time
+import sys
 
 #Data-Processing
 import pandas as pd 
@@ -22,8 +26,48 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
+
+#Metrics
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.metrics import f1_score
+
 #Model Imports
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import IsolationForest
+from sklearn.linear_model import Lasso
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+
+#We import the necessary packages for the Neural Network- we will use PyTorch
+import torch
+from torch import nn
+import torch.nn.functional as F
+import itertools
+
+#We make a class of our model - they are in another file and imported at the end
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+
+#We import the necessary packages for the Neural Network- we will use PyTorch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+#We will first make our Dataset class - this will be useful for the Neural Network
+from torch.utils.data import Dataset
+from modules import WineQualityDataset
+
+#Torch model imports
+from modules import WinePredictorV0, WineModelOptimized, WineModelBig, Somelier
+
+#We will use MLflow to log our experiments
+import mlflow 
+mlflow.set_tracking_uri('http://127.0.0.1:5000')
+
+#Hyperparameter tuning 
+import itertools
+import optuna
+
 
 #%%Data importing 
 df = pd.read_csv('C:/Users/stavr/Desktop/5ο εξάμηνο/statistical learning/Assignment/data/winequality-white.csv', delimiter = ';')
@@ -51,7 +95,6 @@ df_scaled = pd.DataFrame(X_scaled, columns = X.columns)
 #3) For more stability and better predictions we do tree ensembles instead of one tree
 
 #Let's make the model object --------
-from sklearn.ensemble import IsolationForest
 iso_forest_anomaly = IsolationForest(contamination = 0.05) #We use 0.05 for contamination
 #because we are not expecting a very big number of anomalies - may adjust later
 
@@ -189,38 +232,6 @@ top_3_features_logistic = logistic_params.apply(lambda row: row.nlargest(3).to_d
 
 #%%Model 2: Neural Network 
 
-#We import the necessary packages - we will use PyTorch
-
-import torch
-from torch import nn
-
-#We will create our architecture
-import torch.nn.functional as F
-
-#We make a class of our model
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-class WinePredictorV0(nn.Module):
-    def __init__(self, input_shape: int, output_shape: int):
-        super(WinePredictorV0, self).__init__()
-        
-        # Define the architecture using nn.Sequential
-        self.model = nn.Sequential(
-            nn.Linear(in_features=input_shape, out_features=32),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(in_features=32, out_features=16),
-            nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=16, out_features=output_shape)
-        )
-    
-    def forward(self, x):
-        return self.model(x)
-
-
 
 
 #%%PyTorch workflow 
@@ -235,16 +246,6 @@ NNmodelV0 = WinePredictorV0(input_shape = 11, output_shape = 3)
 
 #We will utilise the Dataset and DataLoader utils because they make it more efficient through batching and shuffling
 
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
-
-
-#Utils function creation
-
-#We will first make our Dataset class
-
-from torch.utils.data import Dataset
-from modules import WineQualityDataset
 
 #%%
 def np_to_tensor(arrays): 
@@ -303,12 +304,11 @@ def train_cv_test_split_dataset_and_dataloader(X,y, batch_size = 32):
 
 
 #%%We will make our training loop finally
-from torch import optim
 
-def train_step(data_loader, model=NNmodelV0, task='classification', loss_fn=None, optimizer_fn=None):
+def train_step(data_loader, model, task='classification', loss_fn=None, optimizer_fn=None, learning_rate = 0.01, weight_decay = 0.01):
     # We have to define our loss function and optimizer
     if optimizer_fn is None:
-        optimizer_fn = optim.AdamW(model.parameters(), lr=0.001, weight_decay=2e-2) #This should change
+        optimizer_fn = optim.AdamW(model.parameters(), lr= learning_rate, weight_decay = weight_decay) #This should change
 
     if loss_fn is None:
         loss_fn = nn.CrossEntropyLoss() if task == 'classification' else nn.MSELoss()
@@ -369,7 +369,7 @@ def train_step(data_loader, model=NNmodelV0, task='classification', loss_fn=None
 #%%We will also make our testing step
 
 #We probably can use 
-def test_step(dataloader, model = NNmodelV0, loss_fn = None, task = 'classification'): 
+def test_step(dataloader, model, loss_fn = None, task = 'classification'): 
   '''
   We need to import the test_dataloader here
   '''
@@ -434,10 +434,6 @@ def preprocess_np(X,y):
 
 #%% Making the model
 #Importing the necessary packages 
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-
-# -- We instantiate our model --
-
 #Hyperparameters --------------------------------------------------------
 #We will use the gini impurity criterion - just like we learned in class
 #We will use max_depth = 5 for now to start - we can always expirmenet with this
@@ -470,12 +466,8 @@ def tree_10_times():
 
 #This is our regression part of the assignment and we will make predictions on the quality of the wines
 #Since we will need to do Regression we will import our RMSE function 
-from sklearn.metrics import mean_squared_error as MSE
-
 
 #%% ---------- Model 1: Linear Regression --------------------------------------------------------
-from sklearn.linear_model import Lasso
-
 #We start of with a simple as possible model - a linear regression model
 #The procedure is as simple as it gets. 
 
@@ -518,74 +510,25 @@ def linear_regression_10_times():
         final_array += parameters[i]
         final_array = final_array / 10
         
-    print(f'The mean RMSE of the 10 iterations is {mean_score}')
+    print(f'The mean RMSE of the 10 iterations is {mean_score} \n')
 
     #We turn this into a data-frame for easier reading 
-    final_array = pd.DataFrame(final_array, columns = X.columns)
-    print(f'The mean of the parameters is {final_array}')
-
+    final_array = pd.Series(final_array, index = X.columns).sort_values(ascending = False)
     return mean_score, final_array
 
-#%% 
+#%% Function - Debug - Cell
 linear_regression_10_times()
 
 
-
-#%%We will now make a function that will return the top 3 features for the linear regression model
-
-def linear_regression_top_3_features():
-    #We will make a dictionary that will hold the coefficients of the model
-    #We will then sort it and get the top 3 values
-    coefficients = train_linear_regression_once()
-    coefficients = pd.Series(coefficients, index = X.columns)
-    top_3_features = coefficients.nlargest(3).to_dict()
-
-    return top_3_features
-
 #%%
-#---------------- Model 2: Neural Network --------------------------------------------------------
-
-#We will make a new model for the regression part of the assignment
-
-class Somelier(nn.Module): 
-    def __init__(self, input_shape = int, output_shape = int): 
-        super(Somelier, self).__init__()
-
-        #We now make our architecture
-        self.model = nn.Sequential(
-            nn.Linear(in_features=input_shape, out_features = 64),
-            nn.ReLU(),
-            nn.BatchNorm1d(64), #We add a batch normalization layer --- this first parameter 
-            #is the features that should normalize so we pass 64 in
-            nn.Linear(in_features=64, out_features=128),
-            nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(in_features=128, out_features=64), 
-            nn.ReLU(), 
-            nn.Linear(in_features=64, out_features = output_shape)
-        )
-
-    def forward(self,x): 
-        return self.model(x)
-    
-#Let's make an instance of our model 
-
-SomelierV0 = Somelier(input_shape = 11, output_shape = 1)
-    
-#%% We wanna introduce MLflow logging 
-import mlflow 
-mlflow.set_tracking_uri('http://127.0.0.1:5000')
-
-#%% This package helps us deal with directories and files 
-import os
-import time
+#----------------- Model 2: Neural Network ---------------------------------------------------    
 
 #%% We will now train and test our model 
 
 #We will make a function that trains and tests our model
 #This time we will make it a bit more general
 
-def NN_train_and_test_once_general(epochs, X, y, model, task = 'classification', batch_size = 128, name = None):
+def NN_train_and_test_once_general(epochs, X, y, model, task = 'classification', batch_size = 128, name = None, learning_rate = 0.01, weight_decay = 0.01, model_kwargs = {}):
     METRIC_NAME = 'F1' if task == 'classification' else 'RMSE' #This will be helpful in printing
     #The path can be modified to adopt to the directory that it needs to be saved to
     model_weights_path = f'results/{name}_best_weights.pth' if name is not None else 'results/best_weights.pth'#This will be the path where we save the weights of the model
@@ -613,9 +556,13 @@ def NN_train_and_test_once_general(epochs, X, y, model, task = 'classification',
     #---------- Training the model -----------------------------------
     #We start an MLflow run 
     with mlflow.start_run(run_name = f'Run for Model: {name}'):
+        mlflow.pytorch.autolog() #We autolog the model
         mlflow.log_param('Epoch', epochs) #We log the epochs that we will train on 
         mlflow.log_param('Task', task) #We log the task
-    
+        mlflow.log_param('Batch Size', batch_size) #We log the batch size
+        mlflow.log_param('Model Name', name) #We log the model name
+        mlflow.log_param('Learning Rate', learning_rate) #We log the learning rate
+
         #We start our training loop 
         for epoch in range(epochs):
             print(f'Epoch: {epoch}')
@@ -668,132 +615,63 @@ def NN_train_and_test_once_general(epochs, X, y, model, task = 'classification',
 
     return test_metric
 
-#%%Debug cell 
-
-
-
 #%%
 #We make this function that will train and test n times
 #returning the mean of the metrics which is good practice
 #The rest will be logged in MLflow and we can pull it from there whenever we want it
-def NN_train_test_n_times(epochs, n, X, y, model_class, task='classification', name=None, batch_size=32, **model_kwargs):
+def NN_train_test_n_times(epochs, n, X, y, model_class, task='classification', name=None, batch_size=32, learning_rate = 0.01, **kwargs):
     '''
     Arguments: 
     - model_class || The class of the model that we want to use, NOT an instance.
     - model_kwargs || Any extra arguments needed to initialize the model.
+    e.g. Batch_size, input_shape, output_shape, learning_rate, weight_decay
     '''
+
+    try: 
+        model_kwargs = {'input_shape': kwargs['input_shape'],'output_shape': kwargs['output_shape']}
+    except: 
+        model_kwargs = {'output_shape': kwargs['output_shape']} #In case we don't give an output shape
     
     metrics = []
     for run in range(n):
         model = model_class(**model_kwargs)  # Create a new instance with the given parameters
         print(f'Run: {run}')
         
-        metrics.append(NN_train_and_test_once_general(epochs, X, y, model=model, task=task, name=f'{name}_Run_Number_{run}', batch_size=batch_size))
+        metrics.append(NN_train_and_test_once_general(epochs, X, y, model=model, task=task, name=f'{name}_Run_Number_{run}', batch_size=batch_size, learning_rate = learning_rate))
 
     return np.mean(metrics)
 
-#%% Going to try a new model as well
+#%% We will do some hyper-parameter tuning with Optuna 
 
-class WineModelBig(nn.Module):
-    def __init__(self, input_shape, output_shape, hidden_units=[512, 256, 128, 64]):
-        super(WineModelBig, self).__init__()
+#We define the objective function that optuna wants to optimize 
 
-        layers = []
+#This is in trial phase
 
-        # Input layer -> First hidden layer
-        layers.append(nn.Linear(input_shape, hidden_units[0]))
-        layers.append(nn.ReLU())  # ReLU activation
-        layers.append(nn.BatchNorm1d(hidden_units[0]))  # Batch Normalization for stable training
-        layers.append(nn.Dropout(0.4))  # Dropout for regularization
+def objective(trial, model, X, y, task, output_shape = 3, input_shape = 11):
+    #Suggest values for hyper - parameters 
+    # Suggest values for hyperparameters
+    learning_rate = trial.suggest_loguniform('learning_rate', 1e-4, 1e-1)  # Log scale for LR
+    weight_decay = trial.suggest_loguniform('weight_decay', 1e-6, 1e-2)  # Regularization
+    batch_size = trial.suggest_categorical('batch_size', [32, 64, 128, 256])  # Choose batch size
 
-        # Hidden layers
-        for i in range(len(hidden_units)-1):
-            layers.append(nn.Linear(hidden_units[i], hidden_units[i+1]))
-            layers.append(nn.ReLU())  # ReLU activation
-            layers.append(nn.BatchNorm1d(hidden_units[i+1]))  # Batch Normalization
-            layers.append(nn.Dropout(0.4))  # Dropout for regularization
+    #Call my training and testing function to get the test metric
+    #Optuna with optimize it in regards to this. This is something we define in our methodology.
 
-        # Output layer
-        layers.append(nn.Linear(hidden_units[-1], output_shape))
+    test_metric = NN_train_test_n_times(epochs = 250, n = 1, X = X, y = y, model_class = model, task = task, batch_size = batch_size, learning_rate = learning_rate, weight_decay = weight_decay, output_shape = output_shape, input_shape = input_shape)
 
-        # Sequential model that will stack all the layers
-        self.network = nn.Sequential(*layers)
+    return test_metric #This is what we want to optimize
+# %% Run the optimization
 
-    def forward(self, x):
-        return self.network(x)
-# %% We make an instance
-NNModelV1_Big = WineModelBig(input_shape = 11, output_shape = 3)
-
-#%% -------------------------------------------- Final Pipeline --------------------------------------------
-''''
-We will now make a pipeline that will do the following:
-    - Train and test the classification Neural Network model 10 times and record it in ML flow 
-    - Train and test the regression Neural Network model 10 times and record it in ML flow
-'''
-
-
-#%%------------------ Part 1: Classification ----------------------------------------------------------------
-'''
-We provide
-- epochs: 200 because we saw there was convergence by then for sure
-- n: 10 because we want to do it 10 times
-- X: X_in because we want to use the data with the anomalies deleted
-- y1: y1 because we want to use the data subdivided into classes
-- model: WineModelBig because we want to use the big model
-- name: 'Wine Classifier' because we want to name the model in MLflow
-'''
-
-
+study = optuna.create_study(direction='maximize')  # Create a new study object - maximize because we want to maximize the F1
+study.optimize(lambda trial: objective(trial, WineModelOptimized, X_in_without_outliers, y1_without_outliers, 'classification'), n_trials=100)  # Optimize the study with the objective function
+#%%
 NN_train_test_n_times(epochs = 300, n = 1, X = X_in, y = y1, model_class = WinePredictorV0, task = 'classification', name = 'Wine Classifier', batch_size = 128, input_shape=11, output_shape=3)
+# %% We keyboard interrupt this at epoch 52 
 
-#Note that the ones at the end are kwargs that are passed to the model class to instaniate it with proper parameters
+#Let's obtain the parameters
 
-#We 
-# %% We will do the regression task as well
-
-# ---------- Expirimental, trying once ------------------------------------------------------------------------------
-
-#We will try to use the data with the outliers removed
-NN_train_test_n_times(epochs = 300, n = 1, X = X_in_without_outliers, y = y_without_outliers, model_class = WineModelBig, task = 'regression', name = 'Wine Regressor', batch_size = 128, input_shape = 11, output_shape = 1) 
-# %% Trying a new architecture
-
-import torch
-import torch.nn as nn
-
-class WineModelOptimized(nn.Module):
-    def __init__(self, input_shape, output_shape, hidden_units=[256, 128, 64]):
-        super(WineModelOptimized, self).__init__()
-
-        self.input_layer = nn.Sequential(
-            nn.Linear(input_shape, hidden_units[0]),
-            nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm1d(hidden_units[0]),
-            nn.Dropout(0.2)
-        )
-
-        self.hidden_layers = nn.ModuleList()
-        for i in range(len(hidden_units) - 1):
-            self.hidden_layers.append(nn.Sequential(
-                nn.Linear(hidden_units[i], hidden_units[i+1]),
-                nn.LeakyReLU(negative_slope=0.01),
-                nn.BatchNorm1d(hidden_units[i+1]),
-                nn.Dropout(0.2)
-            ))
-
-        self.output_layer = nn.Linear(hidden_units[-1], output_shape)
-
-    def forward(self, x):
-        x = self.input_layer(x)
-        for layer in self.hidden_layers:
-            x = layer(x)  # Standard forward pass
-        return self.output_layer(x)
-
-# %%
-#We will now train and test this model
-
-NN_train_test_n_times(epochs = 400, n = 3, X = X_in_without_outliers, y = y1_without_outliers, model_class = WineModelOptimized, task = 'classification', name = 'Wine Regressor Optimized', batch_size = 128, input_shape = 11, output_shape = 3)
-# %%
-#Trial run
-NN_train_test_n_times(epochs = 300, n = 1, X = X_in, y = y1, model_class = WinePredictorV0, task = 'classification', name = 'Wine Classifier', batch_size = 128, input_shape=11, output_shape=3)
+classification_params , classification_value = study.best_params, study.best_value
+# %% Let's try a training with this 
+NN_train_test_n_times(epochs = 250, n = 10, X = X_in_without_outliers, y = y1_without_outliers, model_class = WineModelBig, task = 'classification', name = 'Optuna Classifier Trial', learning_rate = 0.04950464437244165 , weight_decay = 0.00023573619467784986, batch_size = 32, input_shape = 11, output_shape = 3)
 
 # %%
